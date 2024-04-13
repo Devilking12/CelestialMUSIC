1758ece758bbc5c56725ec264f960cb965781e61
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2024-04-13 18:59:02 +0530

    Update ping.py

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -20,7 +20,7 @@ async def ping_com(client, message: Message, _):
         video="https://graph.org/file/bf693019ca54a3ad4691e.mp4",
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await DAXX.ping()
+    pytgping = await Devil.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
