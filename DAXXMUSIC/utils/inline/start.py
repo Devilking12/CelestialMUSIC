5df283ce63de7b5611a8ca54850468fd20d8cd0b
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2024-04-13 11:50:23 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/Devil_world_chatting_group"),
         ],
     ]
     return buttons
@@ -25,8 +25,8 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_5"], user_id="6996208572"),
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/feelings_for_you_baby"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
